bdd2cc6883780e0298108fbaeb494b44a4d7eac3
Thomas Feng <huining.feng@gmail.com>
2020-10-04 15:55:56 -0700

    Improve shell scripts.

--- server/bin/shutdown.sh
+++ server/bin/shutdown.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-killall web-alarm-server
+killall web-alarm-server 2> /dev/null
+while pgrep web-alarm-server > /dev/null; do
+  sleep 1;
+done
--- server/bin/startup.sh
+++ server/bin/startup.sh
@@ -2,4 +2,4 @@
 
 dir=$(dirname "$0")
 "$dir/shutdown.sh"
-(cd "$dir/../build" && nohup ./web-alarm-server &)
+(cd "$dir/../build" && nohup ./web-alarm-server > log.txt 2>&1 &)
